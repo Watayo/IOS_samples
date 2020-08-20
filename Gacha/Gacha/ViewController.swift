99df947be063886300f98a75829c44bd382e68da
Watayo <ryo0616mani@gmail.com>
2020-08-20 20:22:01 +0900

    firstCommit

---
+++ Gacha/Gacha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gacha
+//
+//  Created by ryo.w on 2020/08/20.
+//  Copyright © 2020 ryo.w. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
